ec1073cc05fa813ab45078199371341863e351bd
Urvashi <way2urvashi@gmail.com>
2017-08-07 22:47:48 +1000

    TestNG Demo Test

---
+++ src/test/java/com/selenium/testNG/DemoForMaven.java
@@ -0,0 +1,12 @@
+package com.selenium.testNG;
+
+import org.testng.annotations.Test;
+
+public class DemoForMaven {
+	
+	@Test
+	public void testHello() {
+		System.out.println("Welvome to Maven world");
+	}
+
+}
